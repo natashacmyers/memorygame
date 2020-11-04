96164d7146a91aa521e000779bdb4bc48b849067
natashacmyers <natashacmyers@gmail.com>
2020-11-04 20:25:25 +0000

    added animation using javascript onclick functions to the flower images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,18 @@
 const flowers = document.querySelectorAll('.flower');
 
-function flashFlower () {
-    console.log('I was clicked');
-    console.log(this);
+function rotateFlower () {
+    if (this.classList.contains("rotate")) {
+        this.classList.remove("rotate");
+        this.classList.add("rotateAgain");
+    }
+    else if (this.classList.contains("rotateAgain")) {
+        this.classList.remove("rotateAgain");
+        this.classList.add("rotate");
+    }
+    else {
+        this.classList.add("rotate");
+    }
+    
 }
 
-flowers.forEach(flower => flower.addEventListener('click', flashFlower));
+flowers.forEach(flower => flower.addEventListener('click', rotateFlower));
