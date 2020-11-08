c3abb53154c37ee72172da4c8beb87cdac51570f
natashacmyers <natashacmyers@gmail.com>
2020-11-08 11:29:41 +0000

    Added a stop feature to the Start button that envokes a clearing of the level count

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,13 @@
 let order = [];
 let playOrder = [];
 let flash;
-let turn;
+let level;
 let good;
 let compTurn;
 let intervalId;
 let startGame = false;
 let win;
+let stings;
 
 const levelCounter = document.querySelector(".levelcount");
 const yellowFlower = document.querySelector(".yellow-flower");
@@ -38,36 +39,20 @@ function userFlashFlower () {
     
 }
 
-function resetButtonClick () {
-    if (this.classList.contains("resetButtonPressed")) {
-        this.classList.remove("resetButtonPressed");
-        this.classList.add("resetButtonPressedAgain");
-    }
-    else if (this.classList.contains("resetButtonPressedAgain")) {
-        this.classList.remove("resetButtonPressedAgain");
-        this.classList.add("resetButtonPressed");
-    }
-    else {
-        this.classList.add("resetButtonPressed");
-    }
-    
-}
 
 
 function startButtonOn () {
    if (this.classList.contains("startButtonPressed")) {
         this.classList.remove("startButtonPressed");
-        this.classList.add("startButtonPressedAgain");
-        play();
-    }
-    else if (this.classList.contains("startButtonPressedAgain")) {
-        this.classList.remove("startButtonPressedAgain");
-        this.classList.add("startButtonPressed");
-        play();
+        clearInterval(intervalId);
+        levelCounter.innerHTML = 0;
+        document.getElementById("startbutton").innerHTML = "Start";
     }
     else {
         this.classList.add("startButtonPressed");
+        document.getElementById("startbutton").innerHTML = "Stop";
         play();
+        
     }
 }
 
@@ -75,8 +60,22 @@ function startButtonOn () {
 
 flowers.forEach(flower => flower.addEventListener('click', userFlashFlower));
 start.addEventListener('click', startButtonOn);
-reset.addEventListener('click', resetButtonClick);      
+     
 
 
 /* ---------------------- Play --------------- */
 
+function play () {
+win = false;
+order = [];
+playOrder = [];
+flash = 0;
+intervalId = 0;
+level = 1;
+levelCounter.innerHTML = 1;
+for (var i = 0; i <20; i++){
+    order.push(Math.floor(Math.random() * 4) + 1);
+}
+compTurn = true;
+intervalId = setInterval(gameTurn, 800);
+}
