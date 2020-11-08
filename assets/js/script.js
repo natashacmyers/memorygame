e965421f7ff198990c74d14014e347ddac2230ab
natashacmyers <natashacmyers@gmail.com>
2020-11-08 09:44:16 +0000

    Deleted promise and async request, to be replaced with other functions. Added more comments to me JS code for good housekeeping.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
-const flowers = document.querySelectorAll('.flower');
+/* ----------------- Declared Variables ------------------ */
+let order = [];
+let playOrder = [];
+let flash;
+let turn;
+let good;
+let compTurn;
+let intervalId;
+let on = false;
+let win;
 
-function flashFlower () {
+const levelCounter = document.querySelector(".levelcount");
+const yellowFlower = document.querySelector(".yellow-flower");
+const blueFlower = document.querySelector(".blue-flower");
+const greenFlower = document.querySelector(".green-flower");
+const pinkFlower = document.querySelector(".pink-flower");
+const purpleFlower = document.querySelector(".purple-flower");
+const redFlower = document.querySelector(".red-flower");
+/* ----------------- User Buttons ------------------ */
+
+const flowers = document.querySelectorAll('.flower');
+const start = document.querySelector('#startbutton');
+
+function userFlashFlower () {
     if (this.classList.contains("flash")) {
         this.classList.remove("flash");
         this.classList.add("flashAgain");
@@ -15,4 +36,24 @@ function flashFlower () {
     
 }
 
-flowers.forEach(flower => flower.addEventListener('click', rotateFlower));
+
+function startButtonOn () {
+     if (this.classList.contains("startButtonPressed")) {
+        this.classList.remove("startButtonPressed");
+        document.getElementById("startbutton").innerHTML = "Start";
+    }
+    else {
+        this.classList.add("startButtonPressed");
+        document.getElementById("startbutton").innerHTML = "Pause";
+    }
+}
+
+flowers.forEach(flower => flower.addEventListener('click', userFlashFlower));
+start.addEventListener('click', startButtonOn);
+
+
+/* ---------------------- Computer's Turn --------------- */
+
+
+
+/* ----------------------- User's Turn --------------------- */
